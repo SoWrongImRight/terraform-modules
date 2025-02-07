295d20d3b3fe2b6b8f5b2d83ecc928219eca4c80
Russ <dev.ops.rc.1980@gmail.com>
2025-02-07 10:11:06 -0500

    Initial commit.  Added module for vpc setup.

---
+++ modules/vpc/main.tf
@@ -0,0 +1,66 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  tags = {
+    Name = "${var.vpc_name}-${var.environment}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "public" {
+    count = length(var.public_subnets)
+
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.public_subnets[count.index]
+    map_public_ip_on_launch = true
+    availability_zone = element(var.availability_zone, count.index)
+
+    tags = {
+        Name = "public-subnet-${count.index + 1}"
+        Environment = var.environment
+    }
+}
+
+resource "aws_subnet" "private" {
+    count = length(var.private_subnets)
+
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.private_subnets[count.index]
+    availability_zone = element(var.availability_zone, count.index)
+
+    tags = {
+        Name = "private-subnet-${count.indedx + 1}"
+        Environment = var.environment
+    }
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id 
+
+    tags = {
+      Name = "${var.vpc_name}-igw"
+      Environment = var.environment
+    }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.id
+  }
+
+  tags = {
+    Name = "public-route-table1"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets)
+  subnet_id = aws_subnet.public
+  route_table_id = aws_route_table.public.id
+}
