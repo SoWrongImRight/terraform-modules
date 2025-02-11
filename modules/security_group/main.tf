d6b99f27c306ca18e645cdbec2017790c46bdbad
Russ <dev.ops.rc.1980@gmail.com>
2025-02-11 12:55:54 -0500

    Updated security group to include cloudfront.

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -6,6 +6,23 @@ locals {
     tcp_protocol = "tcp"
     all_ips = ["0.0.0.0/0"]
     ssh = 22
+
+    cloudfront_ips = [
+      "13.32.0.0/15",
+      "13.35.0.0/16",
+      "13.54.63.128/26",
+      "13.59.250.0/26",
+      "52.46.0.0/18",
+      "52.84.0.0/15",
+      "54.182.0.0/16",
+      "54.192.0.0/16",
+      "54.230.0.0/16",
+      "54.239.128.0/18",
+      "204.246.164.0/22",
+      "204.246.168.0/22",
+      "204.246.174.0/23",
+      "204.246.176.0/20"
+    ]
 }
 
 resource "aws_security_group" "this" {
@@ -26,14 +43,13 @@ resource "aws_security_group_rule" "allow_http_inbound" {
   cidr_blocks = local.all_ips
 }
 
-resource "aws_security_group_rule" "ssh" {
-  type = "ingress"
+resource "aws_security_group_rule" "allow_https_cloudfront" {
+  type              = "ingress"
   security_group_id = aws_security_group.this.id
-
-  from_port = local.ssh
-  to_port = local.ssh
-  protocol = local.tcp_protocol
-  cidr_blocks = [var.my_ip]
+  from_port         = local.https_port
+  to_port           = local.https_port
+  protocol          = local.tcp_protocol
+  cidr_blocks       = local.cloudfront_ips 
 }
 
 resource "aws_security_group_rule" "allow_all_outbound" {
