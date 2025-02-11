deab949ed5daf20ad0dbee98c74a5ad6fac01710
Russ <dev.ops.rc.1980@gmail.com>
2025-02-11 13:54:01 -0500

    Added route53 to acm.

--- modules/acm/variables.tf
+++ modules/acm/variables.tf
@@ -2,3 +2,8 @@ variable "domain_name" {
   description = "Domain name for SSL certificate"
   type = string
 }
+
+variable "route53_zone_id" {
+  description = "Route 53 Hosted Zone ID"
+  type        = string
+}
