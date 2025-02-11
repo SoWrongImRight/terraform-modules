4f8519648e9fb6b0048d874da7544118f3cb25c7
Russ <dev.ops.rc.1980@gmail.com>
2025-02-11 14:09:31 -0500

    Ironing out acm/route53 bugs.

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -5,4 +5,24 @@ resource "aws_acm_certificate" "this" {
   lifecycle {
     create_before_destroy = true
   }
+
+  options {
+    certificate_transparency_logging_preference = "ENABLED"
+  }
+}
+
+resource "aws_route53_record" "acm_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record.name
+      record = dvo.resource_record.value
+      type   = dvo.resource_record.type
+    }
+  }
+
+  zone_id = var.route53_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
 }
