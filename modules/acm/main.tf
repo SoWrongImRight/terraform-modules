03e355f1f23fdbc502a40560a1826582c5a175bb
Russ <dev.ops.rc.1980@gmail.com>
2025-02-11 15:37:01 -0500

    Fixed variable names.

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -15,9 +15,9 @@ resource "aws_acm_certificate" "this" {
 resource "aws_route53_record" "acm_validation" {
   for_each = {
     for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record.name
-      record = dvo.resource_record.value
-      type   = dvo.resource_record.type
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
     }
   }
 
