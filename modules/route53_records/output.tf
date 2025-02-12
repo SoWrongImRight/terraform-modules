81165c165ff3d4fb9cd6bdd592d88ccbc4e43c83
Russ <dev.ops.rc.1980@gmail.com>
2025-02-12 11:49:38 -0500

    Updated route53_records module.

--- modules/route53_records/output.tf
+++ modules/route53_records/output.tf
@@ -1,9 +1,9 @@
 output "route53_record_names" {
   description = "List of created Route53 record names"
-  value = [for r in aws_route53_record.acm_validation : r.name]
+  value = [for r in aws_route53_record.this : r.name]
 }
 
 output "route53_record_values" {
   description = "List of Route53 record values"
-  value = [ for r in aws_route53_record.acm_validation : r.records ]
+  value = [ for r in aws_route53_record.this : r.records ]
 }
