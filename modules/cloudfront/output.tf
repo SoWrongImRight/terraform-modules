deee48bbe6a1b6a18ffa61a3c93a6ac36e5619bf
Russ <dev.ops.rc.1980@gmail.com>
2025-02-12 15:46:29 -0500

    Updated cloudfront and route53 modules.

--- modules/cloudfront/output.tf
+++ modules/cloudfront/output.tf
@@ -2,3 +2,8 @@ output "cloudfront_dns" {
   description = "Cloudfront DNS name"
   value = aws_cloudfront_distribution.this.domain_name
 }
+
+output "cloudfront_domain_name" {
+  description = "Domain name assigned to Cloudfront distribution"
+  value = aws_cloudfront_distribution.this.domain_name
+}
--- modules/route53_records/output.tf
+++ modules/route53_records/output.tf
@@ -7,3 +7,13 @@ output "route53_record_values" {
   description = "List of Route53 record values"
   value = [ for r in aws_route53_record.this : r.records ]
 }
+
+output "domain_name" {
+  description = "Domain name for website"
+  value = var.domain_name
+}
+
+output "www_domain_name" {
+  description = "The wwww subdomain"
+  value = "wwww.${var.domain_name}"
+}
