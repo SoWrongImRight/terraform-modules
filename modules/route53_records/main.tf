769b88d55a4496f3ed516f814169cd060f6d5455
Russ <dev.ops.rc.1980@gmail.com>
2025-02-12 15:48:52 -0500

    Updated cloudfront and route53 modules.

--- modules/route53_records/main.tf
+++ modules/route53_records/main.tf
@@ -6,3 +6,27 @@ resource "aws_route53_record" "this" {
   records = [each.value.value]
   ttl = 60
 }
+
+resource "aws_route53_record" "cloudfront" {
+  zone_id = var.route53_zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = var.cloudfront_domain_name
+    zone_id                = var.route53_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "cloudfront_www" {
+  zone_id = var.route53_zone_id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = var.cloudfront_domain_name
+    zone_id                = var.route53_zone_id
+    evaluate_target_health = false
+  }
+}
