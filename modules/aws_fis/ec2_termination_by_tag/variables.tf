7b67f21b98ec7fd9fcd374d0405e0ed147ea8a86
Russ Carroll <rcarroll@rcarroll.dev>
2025-05-15 13:28:13 -0400

    Missed variable file updates.

--- modules/aws_fis/ec2_termination_by_tag/variables.tf
+++ modules/aws_fis/ec2_termination_by_tag/variables.tf
@@ -18,3 +18,21 @@ variable "fis_role_arn" {
   description = "ARN of IAM role for FIS"
   type        = string
 }
+
+variable "enable_experiment_report" {
+  description = "Enable experiment report configuratiohn"
+  type = bool
+  default = false
+}
+
+variable "report_s3_bucket_name" {
+  description = "S3 bucket name for FIS reports"
+  type = string
+  default = null
+}
+
+variable "report_dashboard_arn" {
+  description = "Cloudwatch dashboard ARN for FIS experiment" 
+  type = string
+  default = nullß
+}
