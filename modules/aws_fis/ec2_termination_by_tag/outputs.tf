a3637524215025e8bda20a064a74f43fc750f3d8
Russ Carroll <rcarroll@rcarroll.dev>
2025-05-15 10:03:47 -0400

    Updated output for FIS ec2_termination to output the experiment template id, not the id of the resource itself.

--- modules/aws_fis/ec2_termination_by_tag/outputs.tf
+++ modules/aws_fis/ec2_termination_by_tag/outputs.tf
@@ -1,4 +1,4 @@
 output "fis_experiment_template_id" {
   description = "ID of created FIS experiment template."
-  value       = aws_fis_experiment_template.terminate_instances.id
+  value       = replace(aws_fis_experiment_template.terminate_instances.arn, "arn:aws:fis:[^:]+:[^:]+:experiment-template/", "")
 }
