87fda4b4d2dece136268db2ce554c70882ba3ec9
Russ Carroll <rcarroll@rcarroll.dev>
2025-05-15 10:14:11 -0400

    Reverted back.

--- modules/aws_fis/ec2_termination_by_tag/outputs.tf
+++ modules/aws_fis/ec2_termination_by_tag/outputs.tf
@@ -1,4 +1,4 @@
 output "fis_experiment_template_id" {
   description = "ID of created FIS experiment template."
-  value       = replace(aws_fis_experiment_template.terminate_instances.arn, "arn:aws:fis:[^:]+:[^:]+:experiment-template/", "")
+  value       = aws_fis_experiment_template.terminate_instances.id
 }
